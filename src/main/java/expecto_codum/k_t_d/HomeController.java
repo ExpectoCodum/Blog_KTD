95101af2610518ff869371dfe638182f2c54e687
ricky9011 <rpchubukova@gmail.com>
2021-12-29 17:19:04 -0500

    Inicial

--- src/main/java/expecto_codum/k_t_d/HomeController.java
+++ src/main/java/expecto_codum/k_t_d/HomeController.java
@@ -1,14 +1,21 @@
 package expecto_codum.k_t_d;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
-/* aqui falta hacer que cargue el index.html*/
+
+/* aqui falta hacer que cargue el home.html*/
 @Controller
 public class HomeController {
 
+    @RequestMapping("/")
+    public String home() {
+
+
+        return "home"; //view
+    }
 
 
 }
+
+
